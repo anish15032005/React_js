ad849d7b2c5b43d8046a3ff887d5b0b65503defb
Anish Mansuri <146653155+mike-wazoski-1991@users.noreply.github.com>
2024-12-27 17:45:23 +0530

    Code uploaded by on Github.

---
+++ anish-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="card">
+            <img className = "card-img" src="https://media.licdn.com/dms/image/v2/D5603AQEGl0XHcIG89g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1708201901312?e=1740614400&v=beta&t=rG9gWYF2vEOFcfo7XBTMlVL0a8kG1sJshcMNZvmEmfY" alt="image" width="200" height="200"></img>
+            <h2 className="card-title">Anish Mansuri</h2>
+            <p className="card-text">I am a Student and Interested in exploring variuos stuffs.</p>
+        </div>
+    );
+}
+
+export default Card;
